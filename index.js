73d29dab1a232d7cc12ec8065b992885541d7694
Gustavo Souza <gustavo.silva286@fatec.sp.gov.br>
2023-03-03 22:07:56 -0300

    Exercicio1 node

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import PromptSync from "prompt-sync"
-
+import chalk from "chalk"
 const prompt = PromptSync()
-//const prompt = require("prompt-sync")() import promptsync subistitui 
-//const temp = Number(prompt("Digite a temperatura: "))
-//prompt('"Digite"')
-// if(temp > 38)console.log("Febre")
-// else if (temp < 30) console.log("F no chat")
-// else console.log("Tudo ok")
 
-const salario = prompt("Digite seu salário: ")
-const procentagem = Number(prompt("Digite a procentagem de aumento: "))
+const j = prompt("Digite quantos valores vai inserir: ")
+const valores = []
+let soma = 0;
 
-let result = salario*((procentagem/100)+1)
+for (let i = 0; i <j; i++){
 
-console.log(`Seu salário atualizado é: ${result}!`)
+valores[i] = Number(prompt((chalk.bgBlue(`Digite o valor ${i+1}: `))))
+soma = soma + valores[i]
+}
+
+const result = soma/j
+console.log(chalk.bgBlue(`A média é: ${result}!`))
+
+
 
 
