54e8f57b43c20dbf1d3cd9dbb07b4601173012aa
Gustavo Souza <gustavo.silva286@fatec.sp.gov.br>
2023-03-03 21:28:51 -0300

    aula1 node

---
+++ index.js
@@ -0,0 +1,18 @@
+import PromptSync from "prompt-sync"
+
+const prompt = PromptSync()
+//const prompt = require("prompt-sync")() import promptsync subistitui 
+//const temp = Number(prompt("Digite a temperatura: "))
+//prompt('"Digite"')
+// if(temp > 38)console.log("Febre")
+// else if (temp < 30) console.log("F no chat")
+// else console.log("Tudo ok")
+
+const salario = prompt("Digite seu salário: ")
+const procentagem = Number(prompt("Digite a procentagem de aumento: "))
+
+let result = salario*((procentagem/100)+1)
+
+console.log(`Seu salário atualizado é: ${result}!`)
+
+
---
+++ thread.js
@@ -0,0 +1,12 @@
+import PromptSync from "prompt-sync"
+
+const prompt = PromptSync()
+
+const nasc = Number (prompt("Digite o ano que você nasceu: "))
+
+let result = 2023-nasc
+
+if (result < 18)console.log("Você não pode tirar a carteira de motorista, espere um pouco")
+else console.log("Você pode tirar a carta, só vai")
+
+// console.log(`Você está apto a ter a caterira de motorista ${result}!`)
